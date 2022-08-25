02071af5ac15980ae76a268055010eb7ac71a79d
Seaf Aliyan <codo47@gmail.com>
2022-08-25 13:39:26 +0300

    building header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+} from "react-native";
+import React, { useLayoutEffect } from 'react'
+import {useNavigation} from '@react-navigation/native'
 
 function HomeScreen() {
+
+  const navigation = useNavigation()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+      <View>
+        <Image source={{uri:"https://links.papareact.com/wru"}} className="he-7 w-7 bg-gray-300 rounded-full p-4"/>
+      </View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  }
+});
 
 export default HomeScreen;
