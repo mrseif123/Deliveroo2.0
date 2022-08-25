39cb3892113bda557e4ea459168af3e63f57fc46
Seaf Aliyan <codo47@gmail.com>
2022-08-25 21:13:12 +0300

    added dish quantity buttons

--- components/DishRow.js
+++ components/DishRow.js
@@ -2,35 +2,59 @@ import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import Currency from "react-currency-formatter";
 import { urlFor } from '../sanity';
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid';
 const DishRow = ({
   id, name, description, price, image
 }) => {
+    const [isPressed, setIsPressed] = React.useState();
    name = "Peri Pero Nuts"
    description = "Crumchu almonds, cashews and macdamia nuts in a fiery Peri"
    price=4.99
+
+   
   return (
-    <TouchableOpacity className="bg-white border p-4 border-gray-200">
-      <View className="flex-row">
-        <View className="flex-1 pr-2">
-          <Text className="text-l mb-1">{name}</Text>
-          <View></View>
-          <Text className="text-gray-400">{description}</Text>
-          <Text className="text-gray-400 mt-2">
-            <Currency quantity={price} currency="GBP" />
-          </Text>
+    <>
+      <TouchableOpacity
+        onPress={() => setIsPressed(!isPressed)}
+        className={`bg-white border p-4 border-gray-200 ${
+          isPressed && "border-b-0"
+        }`}
+      >
+        <View className="flex-row">
+          <View className="flex-1 pr-2">
+            <Text className="text-l mb-1">{name}</Text>
+            <View></View>
+            <Text className="text-gray-400">{description}</Text>
+            <Text className="text-gray-400 mt-2">
+              <Currency quantity={price} currency="GBP" />
+            </Text>
+          </View>
+          <View>
+            <Image
+              style={{ borderWidth: 1, borderColor: "#f3f3f4" }}
+              className="h-20 w-20 bg-gray-300"
+              source={{
+                // uri: urlFor(image.asset._ref).url
+                uri: "https://links.papareact.com/gn7",
+              }}
+            />
+          </View>
         </View>
-        <View>
-          <Image
-            style={{ borderWidth: 1, borderColor: "#f3f3f4" }}
-            className="h-20 w-20 bg-gray-300"
-            source={{
-              // uri: urlFor(image.asset._ref).url
-              uri: "https://links.papareact.com/gn7",
-            }}
-          />
+      </TouchableOpacity>
+      {isPressed && (
+        <View className="bg-white px-4">
+          <View className="flex-row items-center space-x-2 pb-3">
+            <TouchableOpacity>
+              <MinusCircleIcon color="#00ccbb" size={40} />
+            </TouchableOpacity>
+            <Text>0</Text>
+            <TouchableOpacity>
+              <PlusCircleIcon color="#00ccbb" size={40} />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      )}
+    </>
   );
 }
 
